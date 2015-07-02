f724a2651c82492f59c9fd3c5ef24d97471e93c2
Mauro Cavallin <mauro.cavallin@lemcube.com>
2015-07-02 22:11:21 +0200

    Added a demo winform project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WinFormsSquirrelExample
 An attempt to use squirrel and fluent migration in a winform project
+
+- created a winform project
+-
---
+++ src/LemcubeWinformProgram/LemcubeWinformProgram/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LemcubeWinformProgram
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonCheckForUpdates = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonCheckForUpdates});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(645, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonCheckForUpdates
+            // 
+            this.toolStripButtonCheckForUpdates.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonCheckForUpdates.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonCheckForUpdates.Image")));
+            this.toolStripButtonCheckForUpdates.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonCheckForUpdates.Name = "toolStripButtonCheckForUpdates";
+            this.toolStripButtonCheckForUpdates.Size = new System.Drawing.Size(108, 22);
+            this.toolStripButtonCheckForUpdates.Text = "Check for Updates";
+            this.toolStripButtonCheckForUpdates.Click += new System.EventHandler(this.toolStripButtonCheckForUpdates_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 375);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Lemcube Winform Program";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonCheckForUpdates;
+    }
+}
+
